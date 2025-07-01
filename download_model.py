284c547ad74d9a682001eecefaa0c1ac60466ffc
Stan1ey <1760711322@qq.com>
2025-07-01 18:17:56 +0800

    feat: new_model

--- download_model.py
+++ download_model.py
@@ -1,15 +1,11 @@
-from transformers import AutoTokenizer, AutoModel
+from sentence_transformers import SentenceTransformer
 
 # 定义模型名称和保存目录
-model_name = 'bert-base-chinese'
-save_directory = 'local_bert_model'
-
-# 下载并保存分词器
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-tokenizer.save_pretrained(save_directory)
+model_name = 'shibing624/text2vec-base-chinese'
+save_directory = 'local_text2vec_model'
 
 # 下载并保存模型
-model = AutoModel.from_pretrained(model_name)
-model.save_pretrained(save_directory)
+model = SentenceTransformer(model_name)
+model.save(save_directory)
 
-print(f"模型和分词器已成功下载并保存到 {save_directory} 目录。")
+print(f"模型已成功下载并保存到 {save_directory} 目录。")
--- router/__init__.py
+++
@@ -1,7 +0,0 @@
-# 路由模块初始化
-# 包含API路由定义
-from fastapi import APIRouter
-
-router = APIRouter()
-
-# 此处将包含原main.py中的路由相关代码
